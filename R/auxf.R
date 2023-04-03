e9755e69db1a64d28d803974b5e3e6a4a86c6a14
Karl Tatgenhorst <86030791+k-tatgenhorst@users.noreply.github.com>
2023-04-03 18:06:11 -0400

    Update auxf.R
    Added a seventh and eighth element to ss.col
    The seventh controls the color of the text in text boxes 
    The eight controls the color of the text outside of the boxes

--- R/auxf.R
+++ R/auxf.R
@@ -1,7 +1,8 @@
 # Internal - Prepare canvas for charts
 
 .ss.prepCanvas <- function(main = "Six Sigma graph", sub = "My Six Sigma Project",
-                           ss.col = c("#666666", "#BBBBBB", "#CCCCCC", "#DDDDDD", "#EEEEEE", "#FFFFFF")){
+                           ss.col = c("#666666", "#BBBBBB", "#CCCCCC", "#DDDDDD", "#EEEEEE", 
+                                      "#FFFFFF", "#000000", "#000000")){
   #Plot
   grid::grid.newpage()
   grid::grid.rect(gp = grid::gpar(col = ss.col[2], 
@@ -25,7 +26,7 @@
                              name = "title")
   grid::pushViewport(vp.title)
   grid::grid.text (main, 
-                   gp = grid::gpar(fontsize = 20))
+                   gp = grid::gpar(fontsize = 20, col = ss.col[7]))
   grid::popViewport()
   
   #Subtitle
